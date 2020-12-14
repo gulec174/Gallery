9cbf0e1931400ea9157ee92358f93d991b5e781d
Andrey <fire1993@mail.ru>
2020-12-14 11:33:01 +0300

    Zoom animation was added to Android ImageView.

--- app/src/main/java/com/example/gallery/MyImageView.kt
+++ app/src/main/java/com/example/gallery/MyImageView.kt
@@ -3,7 +3,10 @@ package com.example.gallery
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Rect
+import android.graphics.RectF
 import android.util.AttributeSet
+import android.util.Log
 import android.view.GestureDetector
 import android.view.GestureDetector.SimpleOnGestureListener
 import android.view.MotionEvent
@@ -15,6 +18,8 @@ class MyImageView(context: Context, attributeSet: AttributeSet) :
 
     private var mGestureDetector = GestureDetector(context, GestureListener())
 
+    private val mCurrentViewport = RectF(0f, 0f, 10f, 10f)
+
     private var mScaleDetector: ScaleGestureDetector =
         ScaleGestureDetector(context, ScaleListener())
     private var mScaleFactor: Float = 1f
@@ -22,6 +27,8 @@ class MyImageView(context: Context, attributeSet: AttributeSet) :
     private val kMinBoardScale = 0.1f
     private val kMaxBoardScale = 10f
     private var mBoardScale = 1f
+
+    private val mContentRect: Rect? = null
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent?): Boolean {
@@ -43,22 +50,33 @@ class MyImageView(context: Context, attributeSet: AttributeSet) :
 
     inner class ScaleListener : ScaleGestureDetector.SimpleOnScaleGestureListener() {
         override fun onScale(detector: ScaleGestureDetector): Boolean {
-            val scale = 1 - detector.scaleFactor
-
-            mBoardScale += scale
-
-            if (mBoardScale < kMinBoardScale) mBoardScale = kMinBoardScale
-            if (mBoardScale > kMaxBoardScale) mBoardScale = kMaxBoardScale
+            mBoardScale *= detector.scaleFactor
 
             with(this@MyImageView) {
-                scaleX = 1f / mBoardScale
-                scaleY = 1f / mBoardScale
+                scaleX = mBoardScale
+                scaleY = mBoardScale
             }
             return true
         }
     }
 
     inner class GestureListener : SimpleOnGestureListener() {
+
+
+        override fun onScroll(
+            e1: MotionEvent?,
+            e2: MotionEvent?,
+            distanceX: Float,
+            distanceY: Float
+        ): Boolean {
+            return Math.abs(distanceX) > Math.abs(distanceY) || super.onScroll(
+                e1,
+                e2,
+                distanceX,
+                distanceY
+            )
+        }
+
         override fun onDown(e: MotionEvent): Boolean {
             return false
         }
